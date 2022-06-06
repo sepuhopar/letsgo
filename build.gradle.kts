eafa5917319c36fc07a89da55bc732d39f50ac00
sepuhopar <70010630+sepuhopar@users.noreply.github.com>
2022-06-06 17:23:58 +0300

    Initialize js_apple_silicon

---
+++ build.gradle.kts
@@ -0,0 +1,48 @@
+buildscript {
+    val kotlinVersion: String by project
+    println(kotlinVersion)
+
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+        maven(uri("https://plugins.gradle.org/m2/")) // For kotlinter-gradle
+    }
+
+    dependencies {
+        // keeping this here to allow AS to automatically update
+        classpath("com.android.tools.build:gradle:7.0.4")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${kotlinVersion}")
+        classpath("org.jetbrains.kotlin:kotlin-serialization:${kotlinVersion}")
+
+        with(Deps.Gradle) {
+            classpath(sqlDelight)
+            classpath(shadow)
+            classpath(kotlinter)
+            classpath(gradleVersionsPlugin)
+            val kmpNativeCoroutinesVersion = if (kotlinVersion == "1.6.10") "0.10.0-new-mm" else "0.8.0"
+            classpath("com.rickclephas.kmp:kmp-nativecoroutines-gradle-plugin:$kmpNativeCoroutinesVersion")
+        }
+    }
+}
+
+allprojects {
+    apply(plugin = "org.jmailen.kotlinter")
+
+    repositories {
+        google()
+        mavenCentral()
+        maven(url = "https://maven.pkg.jetbrains.space/kotlin/p/kotlin/kotlin-js-wrappers")
+        maven(url = "https://jitpack.io")
+        maven(url = "https://androidx.dev/snapshots/builds/7888785/artifacts/repository")
+        maven(url = "https://maven.pkg.jetbrains.space/public/p/kotlinx-coroutines/maven")
+        maven(url = "https://maven.pkg.jetbrains.space/public/p/ktor/eap")
+    }
+}
+
+
+// On Apple Silicon we need Node.js 16.0.0
+// https://youtrack.jetbrains.com/issue/KT-49109
+rootProject.plugins.withType(org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootPlugin::class) {
+    rootProject.the(org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension::class).nodeVersion = "16.0.0"
+}
